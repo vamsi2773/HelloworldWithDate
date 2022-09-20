6a245afa1f479381dacf188779d105ee7684c145
vamsi2773 <b.saivamsi@gmail.com>
2022-09-20 11:02:21 +0530

    initial commit

---
+++ src/main/java/com/example/HelloworldWithDate/HelloworldWithDateApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelloworldWithDate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldWithDateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldWithDateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HelloworldWithDate/HelloworldWithDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloworldWithDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldWithDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
